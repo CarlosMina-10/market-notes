404fdf828d29028fa0bcec7327ca871cfd1824ca
CarlosMina-10 <cminav@gmail.com>
2026-03-28 09:24:47 -0400

    Add Google verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,9 @@ export const metadata: Metadata = {
     images: ['/og-image.png'],
     creator: '@marketnotes_co',
   },
+  verification: {
+    google: 'fxvRaE1pdCsZSToF27B',
+  },
   robots: {
     index: true,
     follow: true,
